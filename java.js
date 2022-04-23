57cf1f6dc5887f2cac2b58036333403747b81b83
johnmuninga <johnmuninga03@gmail.com>
2022-04-23 17:44:50 +0200

    commit

--- java.js
+++ java.js
@@ -1,3 +1,6 @@
+// navigation bar javascript starts 
+
+
 let menu = document.querySelector('#menu-btn');
 let navbar = document.querySelector('.navbar');
 
@@ -13,4 +16,30 @@ window.onscroll = () => {
     navbar.classList.remove('active');
     
 }
+
+// navigation bar javascript starts 
+
+
+// statistics section starts
+
+const num = document.querySelectorAll('.num');
+const speed = 200;
+
+num.forEach(item => {
+    const animate = () => {
+        const target = +item.getAttribute('num');
+        const count = +item.textContent;
+
+        const time = target / speed;
+
+        if(count < target) {
+            item.textContent = Math.ceil(count + time);
+            setTimeout(animate, 1);
+        }
+
+    }
+    animate();
+})
+
+// statistics section ends
 
